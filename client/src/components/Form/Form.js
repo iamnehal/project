3994cd414383a0928836145fdf6afc429893aa72
iamnehal <iamnehalchandra@gmail.com>
2021-06-08 01:39:10 +0530

    jaldi dekh

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -3,19 +3,33 @@ import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import FileBase from 'react-file-base64';
 import useStyles from './styles';
-import { createPost} from '../../actions/posts';
+import { createPost,updatePost} from '../../actions/posts';
 
 
-const Form =( ) =>
-{ const dispatch =useDispatch();
+
+
+const Form =({currentId,setCurrentId} ) =>
+{ 
     const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+    const post = useSelector((state) => (currentId ? state.posts.find((p) => p._id === currentId) : null));
+    
+    
+    const dispatch =useDispatch();
+    useEffect(() => {
+      if (post) setPostData(post);
+    }, [post]);
+
     
     const handleSubmit =(e) => {
         e.preventDefault();
- 
-          dispatch(createPost(postData));
-         
-        
+  if(currentId)
+  {
+          dispatch(updatePost(currentId,postData));
+  }   else
+  {   
+  dispatch(createPost(postData));
+       } 
+      
       };
       const clear=()=>
       {
--- server/index.js
+++ server/index.js
@@ -6,15 +6,15 @@ import cors from 'cors';
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+
 app.use('/posts', postRoutes);
 
 
-const CONNECTION_URL = 'mongodb://nehalchandra:niku22021998@cluster0-shard-00-00.7kvvz.mongodb.net:27017,cluster0-shard-00-01.7kvvz.mongodb.net:27017,cluster0-shard-00-02.7kvvz.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-13r5vz-shard-0&authSource=admin&retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 3001;
+const CONNECTION_URL = 'mongodb+srv://nehalchandra:niku22021998@cluster0.7kvvz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 3000;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
